7ec49192e1045b9693aae72b64b4d99f26f848f2
Vasyl <up.vasyl@gmail.com>
2023-01-13 12:11:36 +0200

    fix navigation

--- main.js
+++ main.js
@@ -1,205 +1,89 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+// import './style.css'
+// import * as THREE from 'three'
+// import * as dat from 'lil-gui'
 
-/**
- * Base
- */
-// Debug
-const gui = new dat.GUI()
+// /**
+//  * Debug
+//  */
+// const gui = new dat.GUI()
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+// const parameters = {
+// 	materialColor: '#ffeded'
+// }
 
-// Scene
-const scene = new THREE.Scene()
+// gui
+// 	.addColor(parameters, 'materialColor')
 
-/**
- * Objects
- */
-const object1 = new THREE.Mesh(
-	new THREE.SphereGeometry(0.5, 16, 16),
-	new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-object1.position.x = - 2
+// /**
+//  * Base
+//  */
+// // Canvas
+// const canvas = document.querySelector('canvas.webgl')
 
-const object2 = new THREE.Mesh(
-	new THREE.SphereGeometry(0.5, 16, 16),
-	new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
+// // Scene
+// const scene = new THREE.Scene()
 
-const object3 = new THREE.Mesh(
-	new THREE.SphereGeometry(0.5, 16, 16),
-	new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-object3.position.x = 2
+// /**
+//  * Test cube
+//  */
+// const cube = new THREE.Mesh(
+// 	new THREE.BoxGeometry(1, 1, 1),
+// 	new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// )
+// scene.add(cube)
 
-scene.add(object1, object2, object3)
+// /**
+//  * Sizes
+//  */
+// const sizes = {
+// 	width: window.innerWidth,
+// 	height: window.innerHeight
+// }
 
-/**
- * Raycaster
- */
-const raycaster = new THREE.Raycaster()
-let currentIntersect = null
-const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-const rayDirection = new THREE.Vector3(10, 0, 0)
-rayDirection.normalize()
+// window.addEventListener('resize', () => {
+// 	// Update sizes
+// 	sizes.width = window.innerWidth
+// 	sizes.height = window.innerHeight
 
-// raycaster.set(rayOrigin, rayDirection)
+// 	// Update camera
+// 	camera.aspect = sizes.width / sizes.height
+// 	camera.updateProjectionMatrix()
 
-/**
- * Sizes
- */
-const sizes = {
-	width: window.innerWidth,
-	height: window.innerHeight
-}
+// 	// Update renderer
+// 	renderer.setSize(sizes.width, sizes.height)
+// 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// })
 
-window.addEventListener('resize', () => {
-	// Update sizes
-	sizes.width = window.innerWidth
-	sizes.height = window.innerHeight
+// /**
+//  * Camera
+//  */
+// // Base camera
+// const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+// camera.position.z = 6
+// scene.add(camera)
 
-	// Update camera
-	camera.aspect = sizes.width / sizes.height
-	camera.updateProjectionMatrix()
+// /**
+//  * Renderer
+//  */
+// const renderer = new THREE.WebGLRenderer({
+// 	canvas: canvas
+// })
+// renderer.setSize(sizes.width, sizes.height)
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-	// Update renderer
-	renderer.setSize(sizes.width, sizes.height)
-	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+// /**
+//  * Animate
+//  */
+// const clock = new THREE.Clock()
 
-/**
- * Mouse
- */
-const mouse = new THREE.Vector2()
+// const tick = () => {
+// 	const elapsedTime = clock.getElapsedTime()
 
-window.addEventListener('mousemove', (event) => {
-	mouse.x = event.clientX / sizes.width * 2 - 1
-	mouse.y = - (event.clientY / sizes.height) * 2 + 1
-})
+// 	// Render
+// 	renderer.render(scene, camera)
 
-window.addEventListener('click', () => {
-	if (currentIntersect) {
-		switch (currentIntersect.object) {
-			case object1:
-				console.log('click on object 1')
-				break
+// 	// Call tick again on the next frame
+// 	window.requestAnimationFrame(tick)
+// }
 
-			case object2:
-				console.log('click on object 2')
-				break
-
-			case object3:
-				console.log('click on object 3')
-				break
-		}
-	}
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-	canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-const clock = new THREE.Clock()
-
-const tick = () => {
-	const elapsedTime = clock.getElapsedTime()
-
-	// Animate objects
-	object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
-	object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
-	object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
-
-	// Cast a fixed ray
-	// const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-	// const rayDirection = new THREE.Vector3(1, 0, 0)
-	// rayDirection.normalize()
-
-	// raycaster.set(rayOrigin, rayDirection)
-
-	// const objectsToTest = [object1, object2, object3]
-	// const intersects = raycaster.intersectObjects(objectsToTest)
-
-	// for(const object of objectsToTest)
-	// {
-	//     object.material.color.set('#ff0000')
-	// }
-
-	// for(const intersect of intersects)
-	// {
-	//     intersect.object.material.color.set('#0000ff')
-	// }
-
-	// Cast a ray from the mouse
-	// raycaster.setFromCamera(mouse, camera)
-
-	// const objectsToTest = [object1, object2, object3]
-	// const intersects = raycaster.intersectObjects(objectsToTest)
-
-	// for(const intersect of intersects)
-	// {
-	//     intersect.object.material.color.set('#0000ff')
-	// }
-
-	// for(const object of objectsToTest)
-	// {
-	//     if(!intersects.find(intersect => intersect.object === object))
-	//     {
-	//         object.material.color.set('#ff0000')
-	//     }
-	// }
-
-	// Cast a ray from the mouse and handle events
-	raycaster.setFromCamera(mouse, camera)
-
-	const objectsToTest = [object1, object2, object3]
-	const intersects = raycaster.intersectObjects(objectsToTest)
-
-	if (intersects.length) {
-		if (!currentIntersect) {
-			console.log('mouse enter')
-		}
-
-		currentIntersect = intersects[0]
-	}
-	else {
-		if (currentIntersect) {
-			console.log('mouse leave')
-		}
-
-		currentIntersect = null
-	}
-
-	// Update controls
-	controls.update()
-
-	// Render
-	renderer.render(scene, camera)
-
-	// Call tick again on the next frame
-	window.requestAnimationFrame(tick)
-}
-
-tick()
+// tick()
